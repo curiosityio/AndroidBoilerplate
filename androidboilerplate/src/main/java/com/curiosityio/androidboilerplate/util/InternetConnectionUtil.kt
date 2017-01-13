8dabf9da349e336fc4b4f6d1b4d95e56a631cf04
Levi Bostian <levi@curiosityio.com>
2017-01-12 18:52:33 -0600

    Update internetconnectionutil to be more useful in what its looking for.

--- androidboilerplate/src/main/java/com/curiosityio/androidboilerplate/util/InternetConnectionUtil.kt
+++ androidboilerplate/src/main/java/com/curiosityio/androidboilerplate/util/InternetConnectionUtil.kt
@@ -18,21 +18,35 @@ open class InternetConnectionUtil() {
             return Settings.System.getInt(context.contentResolver, Settings.System.ACCELEROMETER_ROTATION, 0) == 1
         }
 
-        fun isNetworkAvailable(context: Context): Boolean {
+        fun isNetworkConnectedAnyType(context: Context): Boolean {
             val connectivityManager = context.getSystemService(Context.CONNECTIVITY_SERVICE) as ConnectivityManager
             val activeNetworkInfo = connectivityManager.activeNetworkInfo
 
             return activeNetworkInfo != null && activeNetworkInfo.isConnected
         }
 
-        fun isConnectedWifi(context: Context): Boolean {
+        fun isConnectedToWifi(context: Context): Boolean {
+            val connectivityManager = context.getSystemService(Context.CONNECTIVITY_SERVICE) as ConnectivityManager
+            val info = connectivityManager.activeNetworkInfo
+
+            return info != null && info.type == ConnectivityManager.TYPE_WIFI
+        }
+
+        fun isConnectedToWifiAndInternetConnected(context: Context): Boolean {
             val connectivityManager = context.getSystemService(Context.CONNECTIVITY_SERVICE) as ConnectivityManager
             val info = connectivityManager.activeNetworkInfo
 
             return info != null && info.isConnected && info.type == ConnectivityManager.TYPE_WIFI
         }
 
-        fun isConnectedMobile(context: Context): Boolean {
+        fun isConnectedToMobile(context: Context): Boolean {
+            val connectivityManager = context.getSystemService(Context.CONNECTIVITY_SERVICE) as ConnectivityManager
+            val info = connectivityManager.activeNetworkInfo
+
+            return info != null && info.type == ConnectivityManager.TYPE_MOBILE
+        }
+
+        fun isConnectedToMobileAndInternetConnected(context: Context): Boolean {
             val connectivityManager = context.getSystemService(Context.CONNECTIVITY_SERVICE) as ConnectivityManager
             val info = connectivityManager.activeNetworkInfo
 
